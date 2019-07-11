d977a18e539f63ca1cd4bf3ee95c538ebd05cd14
yudawardana <yuda.wardana40@gmail.com>
2019-07-11 20:51:14 +0700

    add youtubeAPI

--- middlewares/errHandler.js
+++ middlewares/errHandler.js
@@ -1,5 +1,15 @@
-function errHandler(err,req,res,next){
+function errHandler(err, req, res, next) {
+    console.log(err)
 
+    let status = null
+    let message = {}
+
+    if (err.code === 404) {
+        status = 404
+        message.err = 'Data Not Found'
+    }
+
+    res.status(status).json(message)
 }
 
 module.exports = errHandler
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,7 @@
 const router = require('express').Router()
+const youtubeRouter =require('./youtubeRouter')
+
+
+router.use('/youtube',youtubeRouter)
 
 module.exports = router
