1b69fd55d67eb441af9ea27c1dd275664b96a00e
yudawardana <yuda.wardana40@gmail.com>
2019-07-11 17:20:01 +0700

    initial file

---
+++ app.js
@@ -0,0 +1,36 @@
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    require('dotenv').config();
+}
+
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const router = require('./routes')
+const errHandler = require('./middlewares/errHandler')
+const app = express()
+
+const port = process.env.PORT
+
+mongoose.connect(`mongodb://localhost:27017/${process.env.DB_NAME}`, {
+    useNewUrlParser: true
+}, (err) => {
+    if (err) {
+        console.log(err), console.log(`Error connection to mongoose`)
+    } else {
+        console.log(`Success connect to mongoose`)
+    }
+})
+
+app.use(cors())
+app.use(express.urlencoded({
+    extended: false
+}))
+app.use(express.json())
+
+app.use('/', router)
+
+app.use(errHandler)
+
+app.listen(port, () => {
+    console.log('Connection Success !!!')
+})
---
+++ middlewares/errHandler.js
@@ -0,0 +1,5 @@
+function errHandler(err,req,res,next){
+
+}
+
+module.exports = errHandler
