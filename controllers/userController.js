f782b4775ab87a6e4397ab0ecbf31b2e9b9848f3
IrsantyoHadi <irsantyohadi@gmail.com>
2019-07-12 11:49:46 +0700

    logic favoritenya udah beres, tinggal lempar2an di clientnnya aja

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,37 +1,38 @@
 const userModel = require('../models/userModel')
+const userFavorite = require('../models/userFavorite')
 const { compare } = require('../helpers/bcrypt')
 const { sign } = require('../helpers/jwtoken')
 
 class UserClass {
     static signup(req, res, next) {
-        let { username, email, password} = req.body
-        let newUser = { username, email, password}
+        let { username, email, password } = req.body
+        let newUser = { username, email, password }
 
         userModel
             .create(newUser)
-            .then((created)=>{
+            .then((created) => {
                 res.status(201).json(created)
             })
             .catch(next)
     }
 
     static signin(req, res, next) {
-        let { username, password} = req.body
+        let { username, password } = req.body
 
         userModel
             .findOne({
                 username: username
             })
-            .then((found)=>{
-                if(found){
-                    if(compare(password, found.password)){
+            .then((found) => {
+                if (found) {
+                    if (compare(password, found.password)) {
                         let payload = {
                             _id: found._id,
                             username: found.username,
                             email: found.email
                         }
                         let token = sign(payload)
-                        res.status(200).json({token})
+                        res.status(200).json({ token })
                     }
                     else {
                         throw `Invalid username / password`
@@ -47,6 +48,31 @@ class UserClass {
 
     static googleLogin(req, res, next) {
 
+    }
+
+    static favorite(req, res, next) {
+        const { label, urlImage, recipe } = req.body
+        let dataToCreate = {
+            // userId = ini harusnya dapet dari req headernya ga sih ? sesuai yang lagi login siapa 
+            label,
+            urlImage,
+            recipe
+        }
+        userFavorite
+            .create(dataToCreate)
+            .then(data => {
+                res.status(201).json(data)
+            })
+            .catch(next)
+    }
+
+    static showFavorite(req, res, next) {
+        userFavorite.find({userId : "5d27fdd80191d810d1cd1e18"}) // ini juga sama kasusnya harusnya dapet dari req header
+        .populate()
+            .then(data => {
+                res.status(200).json(data)
+            })
+            .catch(next)
     }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,5 +4,7 @@ const userController = require('../controllers/userController')
 router.post('/signin', userController.signin)
 router.post('/signup', userController.signup)
 router.post('/login/google', userController.googleLogin)
+router.post('/favorite',userController.favorite)
+router.get('/favorite',userController.showFavorite)
 
 module.exports = router
